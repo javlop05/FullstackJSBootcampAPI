8254bc1bee3f6eb2cd4a23f2221f40d3372f662c
javi <javilopezdelerma84@gmail.com>
2021-11-21 13:08:41 +0100

    Prepare for heroku deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const logger = require('./middlewares/logger');
+const logger = require('./middlewares/logger')
 
 app.use(cors())
 app.use(express.json())
@@ -95,7 +95,8 @@ app.use((request, response) => {
   })
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
