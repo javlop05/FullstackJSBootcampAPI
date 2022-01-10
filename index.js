4401afb43d56e3e91c5bf258e81abf4f08aa9383
javi <javilopezdelerma84@gmail.com>
2022-01-10 20:36:43 +0100

    test: reset endpoint and command to start test server

--- index.js
+++ index.js
@@ -48,6 +48,11 @@ app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(notFound)
 
 app.use(Sentry.Handlers.errorHandler())
